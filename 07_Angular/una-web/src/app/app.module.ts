39fcabeb2954783c51d0d7ce0d9eb46acc73bd0a
Cesar <cesar.rm97@gmail.com>
2019-10-08 09:55:54 +0200

    Angular con lista top de heroes rutas..

---
+++ 07_Angular/una-web/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MiControlComponent } from './mi-control/mi-control.component';
+import { HerosListComponent } from './heros-list/heros-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NewHeroComponent } from './new-hero/new-hero.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiControlComponent,
+    HerosListComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    NewHeroComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
